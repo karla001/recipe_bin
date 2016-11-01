1bae3eefd6124335ad0d0c1be772857769771187
Karla Gilsalazar <karla.gilsalazar@gmail.com>
2016-11-01 16:34:13 -0700

    working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true
-
   has_many :recipes, dependent: :destroy
-  # has_many :time_frames, dependent: :destroy
-  # has_many :tags, through: :recipes, dependent: :destroy
+
+  validates :email, presence: true, uniqueness: true
 
 
 end
