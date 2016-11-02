bde768a402796df52ee1907a8f719cad1d22fef8
Karla Gilsalazar <karla.gilsalazar@gmail.com>
2016-11-02 09:54:11 -0700

    correctected uninitialized constant error in recipe destroy method

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,6 @@ class Recipe < ActiveRecord::Base
 
   belongs_to :user
   has_many :ingredients, dependent: :destroy
-  has_and_belongs_to_many :tags
 
   validates :name, presence: true
   validates :feeds, presence: true
